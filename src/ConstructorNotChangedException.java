0bbd64f6516be52c18f4e6c3aafb6a17c0ca6833
gircin <39699829+gircin@users.noreply.github.com>
2020-09-02 18:46:40 +0300

    Update ConstructorNotChangedException.java
    minor comment fix

--- src/ConstructorNotChangedException.java
+++ src/ConstructorNotChangedException.java
@@ -1,11 +1,11 @@
-/ ** This exception is thrown if after changing the number of enumerations of forms
+/** This exception is thrown if after changing the number of enumerations of forms
  * of the GemForm class, no corresponding changes were made to the constructor of the GemSize class.
  * The GemSize constructor uses GemForm enumerations to define the number of sides to measure.
  * The formsQuantity constant from the GemForm class
  * is used the length of the enumeration array of its class.
  * The same constant in the GemSize class
  * is set manually in the code, to ensure that the constructor is changed.
- * /
+ */
 public class ConstructorNotChangedException extends RuntimeException {
     public ConstructorNotChangedException(String message) {
         super(message);
